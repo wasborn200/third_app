67171a47e20ebd680b3f83c21f3efe1cb641754a
wasborn200 <archibald.ff1@gmail.com>
2019-08-18 14:08:11 +0900

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -20,4 +20,19 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "home display when not logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'title', full_title
+    assert_select 'h1', text: @user.name
+    assert_select 'a>img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+    assert_select 'div.pagination'
+    # @user.microposts.paginate(page: 1).each do |micropost|
+    #   assert_match micropost.content, response.body
+    # end
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
+  end
 end
